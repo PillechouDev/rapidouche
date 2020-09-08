2e31c242c81af19ecd507f97bd173597d2dcdd40
YohanWildz <widogue.yohan@gmail.com>
2020-09-08 15:35:50 +0200

    transfer

--- app/helpers/database.helper.php
+++ app/helpers/database.helper.php
@@ -3,7 +3,7 @@ namespace Helpers;
 
 use PDO;
 function getDatabaseConnection(){
-    $bdd = new PDO('mysql:host=localhost:3308;dbname=projetsite;charset=utf8','root','');
+    $bdd = new PDO('mysql:host=localhost:3308;dbname=rapidouche;charset=utf8','root','');
     return $bdd;
 }
 
